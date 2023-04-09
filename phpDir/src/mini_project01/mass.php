228670d79034d02ce0f2aa7c19367a5d3172e2e1
[Sahil Thapa] <s2300123@BC-C02GQ5FAQ6LR-MacBook-Air.local>
2023-04-09 21:23:09 +0300

    mass conversion

--- phpDir/src/mini_project01/mass.php
+++ phpDir/src/mini_project01/mass.php
@@ -1,8 +1,64 @@
 <?php
+$outputData = 0;
+$inputData = isset($_POST['inputData']);
+$inputFormat = isset($_POST['inputFormat']);
+$outputFormat = isset($_POST['outputFormat']);
 
-/* Mass
-    1.Convert kilograms to grams
-    2.Convert grams to kilograms
- */ 
+switch ($inputFormat){
+    case "kg":
+        switch($outputFormat){
+            case "kg" : $outputData = $inputData;
+            break;
+            case "gram" : $outputData = $inputData*1000;
+            break;
+            case "mg" : $outputData = $inputData*1000000;
+            break;
+        }
+        break;
+    case "gram":
+        switch($outputFormat){
+            case "kg" : $outputData = $inputData/1000;
+            break;
+            case "gram" : $outputData = $inputData;
+            break;
+            case "mg" : $outputData = $inputData*1000;
+            break;
+        }
+        break;
+     case "mg":
+        switch($outputFormat){
+            case "kg" : $outputData = $inputData/1000000;
+            break;
+            case "gram" : $outputData = $inputData/1000;
+            break;
+            case "mg" : $outputData = $inputData;
+            break;
+        }
+        break;
+        default;
+
+}
+
+
 
 ?>
+
+<form action="mass.php" method="post">
+    <label for="inputData">Enter value</label>
+    <input type="number" name="inputData">
+    <label for="inputFormat">From</label>
+    <select name="inputFormat" >
+        <option value="kg" id="kg">Kg</option>
+        <option value="gram" id="gram">Gram</option>
+        <option value="mg" id="mg">Milligram</option>
+    </select>
+    <label for="outputFormat">To</label>
+    <select name="outputFormat" >
+        <option value="kg" id="kg">Kg</option>
+        <option value="gram" id="gram">Gram</option>
+        <option value="mg" id="mg">Milligram</option>
+    </select>
+    <input type="submit" name="submit" value="Convert">
+</form>
+
+<p>Result: <?= $outputData?></p>
