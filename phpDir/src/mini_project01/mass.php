955b3b7c6df0f5c2b918a7c6c8755a1386a54731
[Sahil Thapa] <s2300123@BC-C02GQ5FAQ6LR-MacBook-Air.local>
2023-04-09 22:39:05 +0300

    changed the functionality,now working

--- phpDir/src/mini_project01/mass.php
+++ phpDir/src/mini_project01/mass.php
@@ -1,64 +1,44 @@
 <?php
-$outputData = 0;
-$inputData = isset($_POST['inputData']);
-$inputFormat = isset($_POST['inputFormat']);
-$outputFormat = isset($_POST['outputFormat']);
 
-switch ($inputFormat){
-    case "kg":
-        switch($outputFormat){
-            case "kg" : $outputData = $inputData;
-            break;
-            case "gram" : $outputData = $inputData*1000;
-            break;
-            case "mg" : $outputData = $inputData*1000000;
-            break;
-        }
-        break;
-    case "gram":
-        switch($outputFormat){
-            case "kg" : $outputData = $inputData/1000;
-            break;
-            case "gram" : $outputData = $inputData;
-            break;
-            case "mg" : $outputData = $inputData*1000;
-            break;
-        }
-        break;
-     case "mg":
-        switch($outputFormat){
-            case "kg" : $outputData = $inputData/1000000;
-            break;
-            case "gram" : $outputData = $inputData/1000;
-            break;
-            case "mg" : $outputData = $inputData;
-            break;
-        }
-        break;
-        default;
-
+if(isset($_POST['convert'])) {
+  $weight = $_POST['weight'];
+  $from_unit = $_POST['from_unit'];
+  $to_unit = $_POST['to_unit'];
+  
+  // Conversion factors for various weight units
+  $conversion_factors = [
+    'kg' => 1,
+    'g' => 1000,
+    'mg' => 1000000,
+    'lb' => 2.20462,
+    
+  ];
+  
+  // Convert from the 'from_unit' to kilograms
+  $kg = $weight / $conversion_factors[$from_unit];
+  
+  // Convert from kilograms to the 'to_unit'
+  $result = $kg * $conversion_factors[$to_unit];
+  
+  echo "{$weight}{$from_unit} is equal to {$result}{$to_unit}";
 }
-
-
 
 ?>
 
-<form action="mass.php" method="post">
-    <label for="inputData">Enter value</label>
-    <input type="number" name="inputData">
-    <label for="inputFormat">From</label>
-    <select name="inputFormat" >
-        <option value="kg" id="kg">Kg</option>
-        <option value="gram" id="gram">Gram</option>
-        <option value="mg" id="mg">Milligram</option>
-    </select>
-    <label for="outputFormat">To</label>
-    <select name="outputFormat" >
-        <option value="kg" id="kg">Kg</option>
-        <option value="gram" id="gram">Gram</option>
-        <option value="mg" id="mg">Milligram</option>
-    </select>
-    <input type="submit" name="submit" value="Convert">
+<form method="post">
+  <input type="number" name="weight" placeholder="Enter weight" required>
+  <select name="from_unit">
+    <option value="kg">Kilogram (kg)</option>
+    <option value="g">Gram (g)</option>
+    <option value="mg">Milligram (mg)</option>
+    <option value="lb">Pound (lb)</option>
+  </select>
+  <select name="to_unit">
+    <option value="kg">Kilogram (kg)</option>
+    <option value="g">Gram (g)</option>
+    <option value="mg">Milligram (mg)</option>
+    <option value="lb">Pound (lb)</option>
+  </select>
+  <button type="submit" name="convert">Convert</button>
 </form>
 
-<p>Result: <?= $outputData?></p>
