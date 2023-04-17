6fd1ef9d4da4a86e2860b260a427a5b07eb0352c
Michael Akerele <98211469+aces563@users.noreply.github.com>
2023-04-17 19:36:34 +0300

    renamed default.php andcreated a new index.php as the landing page

--- phpDir/src/index.php
+++ phpDir/src/index.php
@@ -1,23 +1,37 @@
-<?php
-echo "Welcome to PHP World ";
-echo "</br>";  
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Alpha Team Project</h1>
 
+    <main>
+        
+        <section>
+            <h2>Metric</h2>
+            <div class='box metric'>
 
-// The MySQL service named in the docker-compose.yml.
-$host = 'db';
+            </div>
+        </section>
+        <section>
+            <h2>Contact Form</h2>
+            <div class='box contact'>
 
-// Database user name
-$user = 'lionUser';
+            </div>
+        </section>
+        <section>
+            <h2>Todo</h2>
+            <div class='box todo'>
 
-//database user password
-$pass = 'lionPass';
+            </div>
+        </section>
 
-// check the MySQL connection status
-$conn = new mysqli($host, $user, $pass);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected to MySQL server successfully!";
-}
-
-?>
+    </main>
+    
+</body>
+</html>
