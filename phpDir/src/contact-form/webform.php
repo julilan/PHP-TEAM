f2819cbd76579abbd99f6b3165cdf585d16ed61b
Michael Akerele <98211469+aces563@users.noreply.github.com>
2023-04-17 22:36:16 +0300

    added header.php to index.php and webform.php

--- phpDir/src/contact-form/webform.php
+++ phpDir/src/contact-form/webform.php
@@ -53,7 +53,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST") {
 
 ?>
 
-<html lang="en">
+<!-- <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -61,7 +61,9 @@ if($_SERVER["REQUEST_METHOD"]=="POST") {
     <link rel="stylesheet" href="webform.css" media="all">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>    
     <script src="main.js"></script>
-</head>
+</head> -->
+<?php include '../header.php'; ?>
+
 
 <body>
 
---
+++ phpDir/src/header.php
@@ -0,0 +1,31 @@
+<?php
+//header
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>   
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="webform.css" media="all">
+</head>
+<body>
+    
+</body>
+</html>
+
+<header>
+        <h1>Alpha Team Project</h1>
+        <nav>
+            <ul>
+                <li><a href="/">Home</a> </li>
+                <li><a href="/">Metrics</a> </li>
+                <li><a href="/">Todo</a> </li>
+                <li><a href="/contact-form/webform.php">Contact</a> </li>
+
+            </ul>
+        </nav>
+</header>
