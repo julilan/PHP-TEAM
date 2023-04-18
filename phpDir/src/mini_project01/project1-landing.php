8008e330e2b8161dccec56f7bed71b7a4ba80fad
Michael Akerele <98211469+aces563@users.noreply.github.com>
2023-04-18 22:20:38 +0300

    corrected a typo in project1-landing.php

--- phpDir/src/mini_project01/project1-landing.php
+++ phpDir/src/mini_project01/project1-landing.php
@@ -2,7 +2,7 @@
 
 
 <body>
-    <h2>What would you like convert?</h2>
+    <h2>What would you like to convert?</h2>
     <div class="metric-items-container">
         <a href="/mini_project01/mass.php" class="metric-item">Mass</a>
         <a href="/mini_project01/speed.php" class="metric-item">Speed</a>
