94fc57471ab690e9f93753ed95983742c54ab1a5
Sahil Thapa <thapasahil997@gmail.com>
2023-04-20 19:39:59 +0300

    stlyle link in create.php was commented out now fixed

--- phpDir/src/project3-todo/create.php
+++ phpDir/src/project3-todo/create.php
@@ -31,7 +31,7 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 
 
 <?php include '../header.php' ?>
-  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"> -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
   <body>
     <div class="container my-5">
     <form method="post">
